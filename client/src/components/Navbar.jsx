fb1f31ac617ca8d68e8d3ccb89c2314372db0830
varnikag24 <143873214+varnikag24@users.noreply.github.com>
2026-03-28 16:43:31 +0530

    Create Navbar.jsx

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav style={{ padding: "10px", background: "#eee" }}>
+      {/* TODO:
+        Add navigation links
+        Home (/)
+        Posts (/posts)
+        Create (/create)
+      */}
+    </nav>
+  );
+}
+
+export default Navbar;
