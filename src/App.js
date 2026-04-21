296526174ba257bc1967ac258627fe1a68594ec9
darshankrlo <mailtodarshanjain1@gmail.com>
2026-04-21 10:25:01 +0530

    Completed Creator Space Routing Assignment

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React from "react";
-// TODO: Import BrowserRouter, Routes, Route from react-router-dom
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-// TODO: Import pages (Dashboard, Profile, Settings)
+import Dashboard from "./pages/Dashboard";
+import Profile from "./pages/Profile";
+import Settings from "./pages/Settings";
 
 function App() {
   return (
-    <div>
-      {/* TODO: Wrap the application with BrowserRouter */}
-
-      <Navbar />
-
-      {/* TODO: Replace this with Routes
-           "/" → Dashboard
-           "/profile" → Profile
-           "/settings" → Settings
-      */}
-    </div>
+    <BrowserRouter>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/settings" element={<Settings />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,12 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 function Navbar() {
   return (
     <nav style={{ padding: "10px", background: "#eee" }}>
-      {/* TODO: Replace spans with Link/NavLink for routing */}
-      <span style={{ marginRight: "10px" }}>Dashboard</span>
-      <span style={{ marginRight: "10px" }}>Profile</span>
-      <span>Settings</span>
+      <NavLink to="/" style={{ marginRight: "10px" }}>Dashboard</NavLink>
+      <NavLink to="/profile" style={{ marginRight: "10px" }}>Profile</NavLink>
+      <NavLink to="/settings">Settings</NavLink>
     </nav>
   );
 }
